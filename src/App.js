dcce94d24fccae2c6fed50beae00e620ed838e42
Niteshkumar <nitesh156200@gmail.com>
2019-11-22 07:05:59 +0530

    portfolio: Bringup testing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Jumbotron, Button } from "reactstrap";
+
+const Example = () => {
+  return (
+    <div>
+      <Jumbotron>
+        <h1 className="display-3">Hello, there!</h1>
+        <p className="lead">This is my personal website (under construction).</p>
+        <hr className="my-2" />
+        <p>To see my work, please click the button</p>
+        <p className="lead">
+          <Button outline color="primary">
+            <a
+              style={{ textDecoration: "none" }}
+              href="https://www.github.com/niteshkumar2000"
+              target="_blank"
+            >
+              Click here
+            </a>
+          </Button>
+          &nbsp;&nbsp;&nbsp;
+          <Button outline color="primary">
+            <a
+              style={{ textDecoration: "none" }}
+              href="/Resume.pdf"
+              download
+            >
+              Resume
+            </a>
+          </Button>
+        </p>
+      </Jumbotron>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <h2>Hello!!!</h2>
-      <hr/>
-      <h3>Website under construction</h3>
+      <Example />
     </div>
   );
 }
