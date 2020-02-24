6aaec4bb839caebac4142d3fcc15b578b00fbe03
Niteshkumar <nitesh156200@gmail.com>
2020-02-24 10:31:36 +0530

    Portfolio: Components added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,94 @@
 import React from 'react';
 import './App.css';
-import { Button } from "reactstrap";
+import { Nav, NavItem, NavLink, Row, Container, Media, ListGroup, ListGroupItem } from "reactstrap";
 
-const Example = () => {
+
+const Home = () => {
   return (
-    <div>
-        <h1 className="display-3">Hello, there!</h1>
-        <p className="lead">This is my personal website (under construction).</p>
-        <hr className="my-2" />
-        <p>To see my work, please click the button</p>
-        <p className="lead">
-          <Button outline color="primary">
-            <a
-              style={{ textDecoration: "none" }}
-              href="https://www.github.com/niteshkumar2000"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Click here
-            </a>
-          </Button>
-          &nbsp;&nbsp;&nbsp;
-          <Button outline color="primary">
-            <a
-              style={{ textDecoration: "none" }}
-              href="./assets/Resume.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Resume
-            </a>
-          </Button>
-        </p>
-    </div>
+    <React.Fragment>
+          <p style={{fontSize:"50px"}}>
+            Hi, I am <span className="text-primary">Niteshkumar</span>
+          </p>
+
+          <p style={{ fontSize: "35px" }}>
+            I am a software developer. Let's build something amazing together
+          </p>
+    </React.Fragment>
   );
 };
 
+const NavBar = () => {
+  return(
+    <React.Fragment>
+      <Nav className="d-flex p-2 justify-content-end">  
+        <NavItem>
+          <NavLink href="#">
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            About
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            Projects
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            Conatct
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </React.Fragment>
+  )
+}
+
+const About = () => {
+  return(
+    <React.Fragment>
+      <Media className="d-flex align-items-center">
+        <Media left>
+          <Media object data-src="" alt="Profile Photo" />
+        </Media>
+        <Media body className="p-2">
+          <Media heading>
+            About Me
+          </Media>
+          <ListGroup>
+            <ListGroupItem>
+              Name: <span className="text-primary p-2">Niteshkumar S</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Age: <span className="text-primary p-3">20</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Course: <span className="text-primary p-1">&nbsp;MSc Software Systems</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Country: <span className="text-primary p-1">India</span>
+            </ListGroupItem>
+          </ListGroup>
+        </Media>
+      </Media>
+    </React.Fragment>
+  );
+}
 function App() {
   return (
-    <div>
-      <Example />
-    </div>
+    <Container>
+      <Row className="d-flex justify-content-end">
+        <NavBar />
+      </Row>
+      <Row className="d-flex justify-content-center">
+        <Home />
+      </Row>
+      <Row className="d-flex justify-content-start p-5">
+        <About />
+      </Row>
+    </Container>
   );
 }
 
