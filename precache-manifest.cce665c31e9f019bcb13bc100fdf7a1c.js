6ee793506b4a1d6eb0aa6214eb8b2f77c9843b0b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-13 04:15:34 +0000

    Deploy niteshkumar2000/portfolio to github.com/niteshkumar2000/portfolio.git:gh-pages

---
+++ precache-manifest.cce665c31e9f019bcb13bc100fdf7a1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c33c9cce21601364ebec46e03a55b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f43e2f6de6226195b4",
+    "url": "/static/css/2.259ccad4.chunk.css"
+  },
+  {
+    "revision": "69b6b9dac64aff97cf80",
+    "url": "/static/css/main.2a96fcd8.chunk.css"
+  },
+  {
+    "revision": "10f43e2f6de6226195b4",
+    "url": "/static/js/2.b8683fac.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/static/js/2.b8683fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b6b9dac64aff97cf80",
+    "url": "/static/js/main.9682c276.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "88d936f82abeccf13395db75e89934a3",
+    "url": "/static/media/Resume.88d936f8.pdf"
+  },
+  {
+    "revision": "25cad48cbd09ada8457b56e95a05d300",
+    "url": "/static/media/me.25cad48c.jpg"
+  }
+]);
