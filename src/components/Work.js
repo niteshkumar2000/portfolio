7a182a27b8edcd1ec0cf957fcd2c82689f109724
Niteshkumar <nitesh156200@gmail.com>
2022-01-15 14:03:43 +0530

    Portfolio: Update work timeline!
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,6 +11,37 @@ export default class Work extends React.Component {
         <VerticalTimeline>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
+            date="2021 December - Present"
+            contentStyle={{ background: "#1F2022", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  #1F2022" }}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+                <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+              </svg>
+            }
+            iconStyle={{ background: "#ffb800", color: "black" }}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Software Engineer Intern
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">KLA, Chennai</h4>
+            <p style={{ color: "#ffb800" }}>
+              Migrating legacy desktop application to web with Angular and C++
+              back-end.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
             contentStyle={{ background: "#1F2022", color: "#ffb800" }}
             contentArrowStyle={{ borderRight: "7px solid  #1F2022" }}
             date="2021 May - 2021 July"
@@ -60,7 +91,9 @@ export default class Work extends React.Component {
             }
             iconStyle={{ background: "#ffb800", color: "black" }}
           >
-            <h3 className="vertical-timeline-element-title">Student Intern</h3>
+            <h3 className="vertical-timeline-element-title">
+              Software Engineer Intern
+            </h3>
             <h4 className="vertical-timeline-element-subtitle">KLA, Chennai</h4>
             <p style={{ color: "#ffb800" }}>
               (i) Advanced Diagonostics & performance models using Machine
